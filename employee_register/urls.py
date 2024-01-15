99e3eb8a032662e7a39e28fba2f226d4de2a0287
Ondeyo Stephen <ondeyostephen0@gmail.com>
2024-01-15 07:52:47 +0300

    Implemented the complete CRUD functionality

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form, name='employee-form'),
+    path('', views.employee_form, name='employee-insert'),
+    path('<int:id>/', views.employee_form, name='employee-update'),
     path('list/', views.employee_list, name='employee-list'),
+    path('delete/<int:id>/', views.employee_delete, name='employee-delete'),
 ]
